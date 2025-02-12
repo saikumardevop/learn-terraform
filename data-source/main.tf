400b7af898c27fd37a4105c1ac33db4aca7fa0f0
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-13 01:48:32 +0530

    vpc data aws adding

--- data-source/main.tf
+++ data-source/main.tf
@@ -1,22 +1,13 @@
-data "aws_security_group" "selected" {
-  name = "launch-wizard-1"
-}
+//data "aws_security_group" "selected" {
+//  name = "launch-wizard-1"
+//}
+//
+//output "security_group_id" {
+//  value = coalesce(data.aws_security_group.selected.id)
+//}
 
-output "security_group_id" {
+data "aws_security_group" "selected" {}
+
+output "launch-wizard-1" {
   value = data.aws_security_group.selected
 }
-
-//data "aws_security_group" "selected" {}
-//
-//data "aws_security_group" "single" {
-//  count = length(data.aws_security_group.selected.ids)
-//  id = data.aws_security_group.selected.ids[count.index]
-//}
-//
-//output "launch-wizard-1-sg" {
-//  value = data.aws_security_group.selected
-//}
-//
-//output "single" {
-//  value = data.aws_security_group.single
-//}
