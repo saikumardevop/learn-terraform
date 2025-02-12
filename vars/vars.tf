60069b10ab714b20b3901f2a6d0935227eb6b52f
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-12 12:30:16 +0530

    changes frontend

--- vars/vars.tf
+++ vars/vars.tf
@@ -7,10 +7,10 @@ variable "sample_string" {
 # echo $sample_string
 
 output "sample_string" {
-    vale = var.sample_string
+    value = var.sample_string
 }
 
 ## In above case we are printing just variable , If variable is a combination of some strings then following is the syntax.${} is mandatory if we include the variable inside a string
-output "sample_string" {
+output "sample_string1" {
     value = " value of sample_string = ${var.sample_string}"
 }
