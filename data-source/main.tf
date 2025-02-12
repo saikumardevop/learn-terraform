14f665f0c69e046a9221d2239b4ff7aca8eab6b0
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-13 01:31:56 +0530

    vpc data aws adding

--- data-source/main.tf
+++ data-source/main.tf
@@ -3,7 +3,7 @@ data "aws_security_group" "selected" {
 }
 
 output "security_group_id" {
-  value = coalesce(data.aws_security_group.selected)
+  value = data.aws_security_group.selected
 }
 
 //data "aws_security_group" "selected" {}
