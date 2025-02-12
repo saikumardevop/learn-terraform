8765a894d7a62229183106522ca25d6ccc97ccad
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-13 01:27:02 +0530

    vpc data aws adding

--- data-source/main.tf
+++ data-source/main.tf
@@ -7,16 +7,16 @@ output "security_group_id" {
 }
 
 //data "aws_security_group" "selected" {}
-
+//
 //data "aws_security_group" "single" {
 //  count = length(data.aws_security_group.selected)
 //  id = data.aws_security_group.selected[count.index]
 //}
-
+//
 //output "launch-wizard-1-sg" {
 //  value = data.aws_security_group.selected
 //}
-
+//
 //output "single" {
 //  value = data.aws_security_group.single
 //}
