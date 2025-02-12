cdbdea4df0d732f604c13c9d69d413a421e4c564
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-13 01:24:57 +0530

    vpc data aws adding

--- data-source/main.tf
+++ data-source/main.tf
@@ -1,22 +1,22 @@
-//data "aws_security_group" "selected" {
-//  name = "launch-wizard-1"
+data "aws_security_group" "selected" {
+  name = "launch-wizard-1"
+}
+
+output "security_group_id" {
+  value = coalesce(data.aws_security_group.selected)
+}
+
+//data "aws_security_group" "selected" {}
+
+//data "aws_security_group" "single" {
+//  count = length(data.aws_security_group.selected)
+//  id = data.aws_security_group.selected[count.index]
 //}
 
-//output "security_group_id" {
-//  value = coalesce(data.aws_security_group.selected)
+//output "launch-wizard-1-sg" {
+//  value = data.aws_security_group.selected
 //}
 
-data "aws_security_group" "selected" {}
-
-data "aws_security_group" "single" {
-  count = length(data.aws_security_group.selected.id)
-  id = data.aws_security_group.selected.id[count.index]
-}
-
-output "launch-wizard-1-sg" {
-  value = data.aws_security_group.selected.id
-}
-
-output "single" {
-  value = data.aws_security_group.single
-}
+//output "single" {
+//  value = data.aws_security_group.single
+//}
