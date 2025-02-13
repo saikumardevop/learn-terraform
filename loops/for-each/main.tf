2ac5f0d8968fb0311314760bec82eaf5e0ebb8b4
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-14 00:33:04 +0530

    count fruits

--- loops/for-each/main.tf
+++ loops/for-each/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
 resource "null_resource" "fruits" {
 
   for_each = var.fruits
@@ -6,8 +14,18 @@ resource "null_resource" "fruits" {
     command = "echo Fruit Name - ${each.key} - ${each.value}"
     //command = "echo ${length(var.fruits)}"
   }
+
 }
 
+resource "null_resource" "fruits1" {
+
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key} - ${each.value["count"]}"
+    //command = "echo ${length(var.fruits)}"
+  }
+}
 
 variable "fruits1" {
   default = {
