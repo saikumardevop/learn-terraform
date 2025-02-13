5ae9ce1c171c3e513235cb0056bbd188a8ebfe77
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-14 00:17:18 +0530

    count fruits

--- loops/for-each/main.tf
+++ loops/for-each/main.tf
@@ -3,25 +3,33 @@ resource "null_resource" "fruits" {
   for_each = var.fruits
 
   provisioner "local-exec" {
-    command = "echo Fruit Name - ${var.fruits[each.value["name"]]}
+    command = "echo Fruit Name - ${each.key} - ${each.value}"
     //command = "echo ${length(var.fruits)}"
   }
 }
 
 
+# #variable "fruits" {
+#   default = {
+#     apple = {
+#       name  = "apple"
+#       count = 10
+#     }
+#     orange = {
+#       name  = "orange"
+#       count = 200
+#     }
+#     banana = {
+#       name  = "banana"
+#       count = 100
+#     }
+#   }
+# }
+
 variable "fruits" {
   default = {
-    apple = {
-      name  = "apple"
-      count = 10
-    }
-    orange = {
-      name  = "orange"
-      count = 200
-    }
-    banana = {
-      name  = "banana"
-      count = 100
-    }
+    apple = 10
+    orange = 200
+    banana = 100
   }
 }
