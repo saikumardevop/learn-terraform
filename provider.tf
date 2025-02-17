135b04f66b190519aba65bfde2e670404dc00320
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-17 16:04:15 +0530

    count fruits

--- provider.tf
+++ provider.tf
@@ -4,19 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.87.0"
     }
-
-    backend = "s3"
-    config = {
-      bucket = "terraform-g20"
-      key    = "sai/terraform.tfstate"
-      region = "us-east-1"
-    }
-
   }
+  backend "s3" {
+
+    bucket = "terraform-g20"
+    key    = "sai/terraform.tfstate"
+    region = "us-east-1"
+
   }
 
 }
 
 provider "aws" {
   # Configuration options
+
 }
