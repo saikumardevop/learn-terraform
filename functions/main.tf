21f15b403a8c2f9553332b6f6d67f7697cd80395
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-19 15:56:57 +0530

    count fruits

--- functions/main.tf
+++ functions/main.tf
@@ -11,8 +11,19 @@ variable "sample1" {
   default = [ "abc", "xyz"]
 }
 
+variable "sample2" {
+  default = {
+    xyz = 300
+    abc = 200
+  }
+}
+
 ## Print first value like var.sample1[0] with a function
 
 output "sample1" {
-  value = (var.sample1)
+  value = element(var.sample1, 0)
+}
+
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
 }
